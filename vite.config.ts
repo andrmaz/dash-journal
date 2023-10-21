d34f17fc085c0378b649b2921d60942f46cf8215
andrmaz <60042277+andrmaz@users.noreply.github.com>
2023-10-21 17:09:38 +0200

    fix(test): upgrade vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import type { UserConfigExport } from 'vite'
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
-const config: UserConfigExport = defineConfig({
+export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
@@ -19,5 +18,3 @@ const config: UserConfigExport = defineConfig({
     },
   },
 })
-
-export default config
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,5 @@
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import {defineConfig} from 'vitest/config'
-import {mergeConfig} from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
